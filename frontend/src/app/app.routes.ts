076b009045a7c8cb49fe1d1353d6eeb28223a54b
gpodergajs <gpodergajs@gmail.com>
2025-09-19 19:42:05 +0200

    Update app.routes.ts

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,7 +1,8 @@
 import { Routes } from '@angular/router';
 import { LayoutComponent } from './core/components/layout/layout.component';
 import { CarListPageComponent } from './features/car/pages/car-list-page/car-list-page.component';
-
+import { CarDetailsComponent } from './features/car/components/car-details/car-details.component';
+import { EditCarComponent } from './features/car/components/edit-car/edit-car.component';
 
 export const routes: Routes = [
   {
@@ -9,6 +10,9 @@ export const routes: Routes = [
     component: LayoutComponent, // layout wraps everything
     children: [
       { path: 'cars', component: CarListPageComponent },
+      { path: 'cars/:id', component: CarDetailsComponent }, // Route with car ID
+      { path: 'cars/:id/edit', component: EditCarComponent }, // Route for editing car
+
       /*{ path: 'cars' },
       { path: 'cars/:id', component: CarDetailComponent },
       { path: 'login', component: LoginComponent },*/
