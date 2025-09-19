9f946e612614be4f026e67dcc1ccb33251b0b456
gpodergajs <gpodergajs@gmail.com>
2025-09-19 17:00:53 +0200

    modified filtering and pagination

--- frontend/src/app/features/car/components/car-data-view/car-data-view.component.ts
+++ frontend/src/app/features/car/components/car-data-view/car-data-view.component.ts
@@ -10,8 +10,7 @@ import { PaginatorComponent } from "../../../../shared/components/paginator/pagi
 import { MatCardModule } from "@angular/material/card";
 import { MatAutocompleteModule } from "@angular/material/autocomplete";
 import { FormsModule } from "@angular/forms";
-// If you are no longer using <app-filter>, remove FilterComponent
-// import { FilterComponent } from "../../../../shared/components/filter/filter.component";
+
 
 @Component({
   selector: 'app-car-data-view',
@@ -36,49 +35,5 @@ import { FormsModule } from "@angular/forms";
 })
 export class CarDataViewComponent implements OnInit {
   @Input() cars: Car[] = [];
-
-  layout: string = 'grid';
-  options = ['list', 'grid'];
-
-  // Pagination properties
-  totalCars = 0;
-  pageSize = 10;
-  pageIndex = 0;
-  pageSizeOptions = [5, 10, 25, 100];
-
-  // Filter property
-  filters = {
-    priceFrom: null,
-    priceTo: null,
-    mileage: null,
-    year: null
-  };
-
-  years: number[] = [];
-  filteredYears: number[] = [];
-
-  constructor(private carApi: CarApiService) {}
-
-  ngOnInit(): void {
-    const currentYear = new Date().getFullYear();
-    this.years = Array.from({ length: 30 }, (_, i) => currentYear - i); // last 30 years
-    this.filteredYears = [...this.years];
-  }
-
-  onPageChange(event: PageEvent) {
-    this.pageIndex = event.pageIndex;
-    this.pageSize = event.pageSize;
-  }
-
-  applyFilter() {
-    console.log('Filters applied:', this.filters);
-    // TODO: call API with this.filters and update `this.cars`
-  }
-
-  clearFilter() {
-    this.filters = { priceFrom: null, priceTo: null, mileage: null, year: null };
-    this.filteredYears = [...this.years];
-    console.log('Filters cleared');
-    // TODO: reload all cars
-  }
+  ngOnInit(): void {}
 }
--- frontend/src/app/features/car/pages/car-list-page/car-list-page.component.ts
+++ frontend/src/app/features/car/pages/car-list-page/car-list-page.component.ts
@@ -4,27 +4,101 @@ import { Observable, of } from 'rxjs';
 import { catchError, map } from 'rxjs/operators';
 import { CommonModule } from '@angular/common';
 import { CarDataViewComponent } from '../../components/car-data-view/car-data-view.component';
+import { FormsModule } from '@angular/forms';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { PaginatorComponent } from '../../../../shared/components/paginator/paginator.component';
 
 @Component({
   selector: 'app-car-list-page',
   standalone: true,
-  imports: [CommonModule, CarDataViewComponent],
+  imports: [CommonModule,   CommonModule,
+    MatCardModule,
+    MatButtonModule,
+    // If you are no longer using <app-filter>, remove FilterComponent
+    // FilterComponent,
+    PaginatorComponent,
+    CarDataViewComponent,
+    // Angular Material form controls
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatAutocompleteModule,
+    FormsModule],
   templateUrl: './car-list-page.component.html',
   styleUrl: './car-list-page.component.scss'
 })
 export class CarListPageComponent implements OnInit {
-  cars$: Observable<Car[]> = of([]);
+cars$: Observable<Car[]> = of([]);
 
-  constructor(private carApi: CarApiService) { }
+  // Pagination properties
+  totalCars = 0;
+  pageSize = 10;
+  pageIndex = 0;
+  pageSizeOptions = [5, 10, 25, 50];
+
+  // Filters
+ filters: { priceFrom?: number; priceTo?: number; mileage?: number; year?: number } = {
+  priceFrom: undefined,
+  priceTo: undefined,
+  mileage: undefined,
+  year: undefined
+};
+  priceOptions = [5000, 10000, 15000, 20000, 30000, 50000];
+  mileageOptions = [5000, 10000, 20000, 50000, 100000];
+  years: number[] = [];
+
+  constructor(private carApi: CarApiService) {}
 
   ngOnInit(): void {
-    this.cars$ = this.carApi.getCars(1, 10).pipe(
-      map(response => response.cars),
-      catchError(error => {
-        console.error('Error fetching cars:', error);
-        return of([]); // Return an empty array on error
+    const currentYear = new Date().getFullYear();
+    this.years = Array.from({ length: 30 }, (_, i) => currentYear - i);
+    
+    // Load first page
+    this.loadCars();
+  }
+
+  loadCars() {
+    this.cars$ = this.carApi.getCars(this.pageIndex + 1, this.pageSize, this.filters).pipe(
+      map(response => {
+        this.totalCars = response.total_items; // Update totalCars for paginator
+        return response.cars;
+      }),
+      catchError(err => {
+        console.error('Error fetching cars:', err);
+        return of([]);
       })
     );
   }
+
+  applyFilter() {
+    this.pageIndex = 0; // Reset to first page when filters change
+    this.loadCars();
+    console.log('Filters applied:', this.filters);
+  }
+
+  clearFilter() {
+     this.filters = {
+    priceFrom: undefined,
+    priceTo: undefined,
+    mileage: undefined,
+    year: undefined
+  };
+    this.pageIndex = 0;
+    this.loadCars();
+    console.log('Filters cleared');
+  }
+
+  onPageChange(event: any) {
+    this.pageIndex = event.pageIndex;
+    this.pageSize = event.pageSize;
+    this.loadCars();
+  }
+
 }
+
 
